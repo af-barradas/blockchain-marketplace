58462cd1072d5e6c5462697a7fe325db80dcef0f
unknown <afbarradas@gmail.com>
2022-04-06 18:41:40 +0100

    transfer not working

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -13,11 +13,19 @@ contract MarketPlace {
         uint256 id;
         string name;
         uint256 price;
-        address owner;
+        address payable owner;
         bool isAvailable;
     }
 
     event ProductCreated(
+        uint256 id,
+        string name,
+        uint256 price,
+        address owner,
+        bool isAvailable
+    );
+
+    event ProductPurchased(
         uint256 id,
         string name,
         uint256 price,
@@ -30,16 +38,38 @@ contract MarketPlace {
     }
 
     function createProduct(string memory _name, uint256 _price) public {
+        require(bytes(_name).length > 0);
+        require(_price > 0);
+
         productCount++;
 
         products[productCount] = Product(
             productCount,
             _name,
             _price,
-            msg.sender,
+            payable(msg.sender),
             true
         );
 
         emit ProductCreated(productCount, _name, _price, msg.sender, true);
+    }
+
+    function purchaseProduct(uint256 _id) public payable {
+        Product memory product = products[_id];
+        address payable seller = product.owner;
+
+        product.owner = payable(msg.sender);
+        product.isAvailable = false;
+        products[_id] = product;
+
+        seller.transfer(msg.value);
+
+        emit ProductPurchased(
+            productCount,
+            product.name,
+            msg.value,
+            product.owner,
+            product.isAvailable
+        );
     }
 }
--- test/marketPlace.test.js
+++ test/marketPlace.test.js
@@ -1,5 +1,9 @@
 const MarketPlace = artifacts.require('MarketPlace')
 
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
 contract('MarketPlace', ([deployer, seller, buyer]) => {
     let marketPlace
 
@@ -38,6 +42,39 @@ contract('MarketPlace', ([deployer, seller, buyer]) => {
             assert.equal(event.price, web3.utils.toWei('1', 'Ether'), 'price is correct')
             assert.equal(event.owner, seller, 'owner is correct')
             assert.equal(event.isAvailable, true, 'isAvailable is correct')
+
+            await marketPlace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected
+            await marketPlace.createProduct('Samsung M12 Sky Blue', 0, { from: seller }).should.be.rejected
+        })
+
+        it('last product saved successfully', async () => {
+            product = await marketPlace.products(productCount)
+            assert.equal(product.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(product.name, 'Samsung M12 Sky Blue', 'name is correct')
+            assert.equal(product.price, web3.utils.toWei('1', 'Ether'), 'price is correct')
+            assert.equal(product.owner, seller, 'owner is correct')
+            assert.equal(product.isAvailable, true, 'isAvailable is correct')
+        })
+
+        it('purchased product successfully', async () => {
+            let oldSellerBalance = await web3.eth.getBalance(seller)
+            oldSellerBalance = new web3.utils.BN(oldSellerBalance)
+
+            result = await marketPlace.purchaseProduct(productCount, {
+                from: buyer, value: web3.utils.toWei('1', 'Ether')
+            })
+            let event = result.logs[0].args
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(event.name, 'Samsung M12 Sky Blue', 'name is correct')
+            assert.equal(event.price, web3.utils.toWei('1', 'Ether'), 'price is correct')
+            assert.equal(event.owner, buyer, 'owner is correct')
+            assert.equal(event.isAvailable, false, 'isAvailable is correct')
+
+            let newSellerBalance = await web3.eth.getBalance(seller)
+            newSellerBalance = new web3.utils.BN(newSellerBalance)
+            let price = web3.utils.toWei('1', 'Ether')
+            price = new web3.utils.BN(price)
+            console.log(oldSellerBalance, newSellerBalance, price)
         })
     })
 })
